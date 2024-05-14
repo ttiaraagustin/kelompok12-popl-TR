c9e0da9f3cb82d616acaafc631f82c59ce96d1d8
Andika Pebriansyah <andikaa22@mhs.usk.ac.id>
2024-05-15 00:14:47 +0700

    create migration and seeder

--- database/migrations/2024_05_14_105708_create_users_table.php
+++ database/migrations/2024_05_14_105708_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama_lengkap');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->enum('level', ['ADMIN', 'DOKTER']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
