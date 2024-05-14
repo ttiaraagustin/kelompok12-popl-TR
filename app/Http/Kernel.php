64c91f3822e4d7b4b1f09168c695e559a76663b9
Andika Pebriansyah <andikaa22@mhs.usk.ac.id>
2024-05-15 00:03:50 +0700

    custom kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        //* -CUSTOM-
+        'isAdmin' => \App\Http\Middleware\isAdmin::class,
+        'isDokter' => \App\Http\Middleware\isDokter::class,
+        'isAdminAndDokter' => \App\Http\Middleware\isAdminAndDokter::class,
     ];
 }
